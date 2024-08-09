d9efda3845423f5cc3bc9955c189c38ab694538a
badripokhrel77 <badripokhrel.77@gmail.com>
2024-08-09 15:43:23 +0545

    first commit

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>hotel room reservation system</title>
+    <link rel="stylesheet" href="{{url('css/nav.css')}}">
+
+</head>
+<body>
+    <header>
+        <div class="nav">
+            <div class="logo">
+                <img src="../images/HRRS logo HD.webp" alt="HRRS">
+                <h4>Hotel Room Booking</h4>
+            </div>
+            <div class="navlinks">
+                <ul id="menulist">
+                    <li> <a href="..//homepage/home.php">Home</a></li>
+                    <li> <a href="#about"> About</a></li>
+                    <li> <a href="#rooms">Rooms</a></li>
+                    <li> <a href="#service">Services</a></li>
+                    <li> <a href="#contact">Contact</a></li>
+                    <li> <a href="..//Login/loginForm.php">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    </body>
+    </html>
+    
